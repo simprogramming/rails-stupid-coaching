82f2adcbed2c58467b9f2cb7b160d6bb6f8d984c
Simon Leiva <leivaprogramming@gmail.com>
2020-05-11 16:59:28 -0400

    not sure I have commited all the good files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == nil || ""
+      @question = 'Nothing...'
+      @answer = 'Nothing...'
+    else
+      @question = params[:question]
+      if params[:question] == "I am going to work"
+        @answer = "Great!"
+      elsif @question.include?('?')
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed!"
+      end
+    end
+  end
+end
